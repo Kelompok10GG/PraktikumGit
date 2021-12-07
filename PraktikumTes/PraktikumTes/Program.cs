d264a74c707f516ea31037cba206f98e0fb0c24f
hilal <hilalgoodluck@gmail.com>
2021-12-07 20:11:24 +0700

    Hilal merubah program.cs

--- PraktikumTes/PraktikumTes/Program.cs
+++ PraktikumTes/PraktikumTes/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Kelompok 10");
+
+Console.WriteLine("Praktikum membuat Git");
