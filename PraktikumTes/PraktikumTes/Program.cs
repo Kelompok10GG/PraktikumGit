72a5b8935294974283b47417a1042d887e710eb0
C-Yonzz <panipermana10@students.amikom.ac.id>
2021-12-07 21:17:48 +0700

    Menambahkan Nim,Nama

--- PraktikumTes/PraktikumTes/Program.cs
+++ PraktikumTes/PraktikumTes/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Kelompok 10");
-Console.WriteLine("Cobaa");
 Console.WriteLine("Anggota kelompok 10");
+Console.WriteLine("Pani Permana 20.11.34311");
