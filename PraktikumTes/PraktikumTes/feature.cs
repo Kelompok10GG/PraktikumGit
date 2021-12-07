844513f6e58e7ebb3ae75bba07c84f0f5e443401
hilal <hilalgoodluck@gmail.com>
2021-12-07 20:20:59 +0700

    menambah code program pada feature.cs

--- PraktikumTes/PraktikumTes/feature.cs
+++ PraktikumTes/PraktikumTes/feature.cs
@@ -8,5 +8,17 @@ namespace PraktikumTes
 {
     internal class feature
     {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Masukan Nama = \t");
+            System.Console.ReadLine();
+            System.Console.Write("Masukan NIM = \t");
+            System.Console.ReadLine();
+            System.Console.Write("Masukan Jurusan = \t");
+            System.Console.ReadLine();
+            System.Console.Write("Masukan Alamat = \t");
+            System.Console.ReadLine();
+        }
     }
 }
+
