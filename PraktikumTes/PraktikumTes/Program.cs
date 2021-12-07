af9eb2713b2ccf83017f7343b0c6f4ccdac0a452
DimasTurbo <tersediabakso@gmail.com>
2021-12-07 21:22:49 +0700

    Nama Anggota Kelompok

--- PraktikumTes/PraktikumTes/Program.cs
+++ PraktikumTes/PraktikumTes/Program.cs
@@ -2,4 +2,8 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Kelompok 10");
 Console.WriteLine("Anggota kelompok 10");
-Console.WriteLine("Pani Permana 20.11.34311");
+Console.WriteLine("Pani Permana 20.11.3411");
+Console.WriteLine("Nurfauzan Bimantoro 20.11.3432");
+Console.WriteLine("Dimas Zaldiyanto 20.11.3433");
+Console.WriteLine("Rifqi Hilal Bhahari 20.11.3434");
+Console.WriteLine("Ammar Kanz Diwani 20.11.3435");
